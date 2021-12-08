f342508afac85a8be3b8d4660f84c6ea972c669c
Dari0L <48294644+Dari0L@users.noreply.github.com>
2021-12-08 18:55:39 +0100

    prima configurazione

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static APL_software.connection;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        //connection prova= new connection();
+
+        Socket sender= APL_software.connection.StartClient();
+        //Console.WriteLine("Hello, World!");
+/*
+// Encode the data string into a byte array.
+            byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+            // Send the data through the socket.
+            int bytesSent = sender.Send(msg);
+
+            // Receive the response from the remote device.
+            int bytesRec = sender.Receive(bytes);
+            Console.WriteLine("Echoed test = {0}",
+                Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+            // Release the socket.
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+*/
+
+    }
+
+}
+
+
+
+
+
+
+
---
+++ connection.cs
@@ -0,0 +1,66 @@
+﻿namespace APL_software;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public static class connection
+{
+
+    public static Socket  StartClient()
+    {
+        
+
+        try
+        {
+            // Connect to a Remote server
+            // Get Host IP Address that is used to establish a connection
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+            // If a host has multiple addresses, you will get a list of addresses
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP  socket.
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                // Connect to Remote EndPoint
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}",
+                    sender.RemoteEndPoint.ToString());
+
+                return sender;
+               
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+
+        return null;
+    }
+
+  
+
+}
