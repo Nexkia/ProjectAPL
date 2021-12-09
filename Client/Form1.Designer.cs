392c4fd8ba66d0e61b07410b3f4f72c2c2bbb361
Dari0L <48294644+Dari0L@users.noreply.github.com>
2021-12-09 19:33:24 +0100

    client invio tramite socket di una stringa

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -29,21 +29,31 @@ namespace Client
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -8,17 +8,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Net;
+using System.Net.Sockets;
+
+
+
+
 namespace Client
 {
+     
     public partial class Form1 : Form
     {
+        Socket send;
         public Form1()
         {
+            
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+             send = Connection.StartClient();
+            Console.WriteLine("load caricata correttamente");
+            
+            Console.WriteLine("Socket connected to {0}",
+                    send.RemoteEndPoint.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("prova1");
+
+            byte[] bytes = new byte[1024];
+
+            // Encode the data string into a byte array.
+            byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+            // Send the data through the socket.
+            int bytesSent = send.Send(msg);
+
+            // Receive the response from the remote device.
+            int bytesRec = send.Receive(bytes);
+            Console.WriteLine("Echoed test = {0}",
+                Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+
+
+
+            send.Shutdown(SocketShutdown.Both);
+            send.Close();
 
         }
     }
