a9d513701c87d39bd2e34c2ee30d3aaa1d1cd373
Dari0L <48294644+Dari0L@users.noreply.github.com>
2021-12-10 19:53:25 +0100

    login (email, password) + select database my sql

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -30,30 +30,97 @@ namespace Client
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Button();
+            this.Register = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonMostra = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // Login
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            resources.ApplyResources(this.Login, "Login");
+            this.Login.Name = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Register
+            // 
+            resources.ApplyResources(this.Register, "Register");
+            this.Register.Name = "Register";
+            this.Register.UseVisualStyleBackColor = true;
+            this.Register.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Password
+            // 
+            resources.ApplyResources(this.Password, "Password");
+            this.Password.BackColor = System.Drawing.SystemColors.Control;
+            this.Password.Name = "Password";
+            this.Password.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxEmail
+            // 
+            resources.ApplyResources(this.textBoxEmail, "textBoxEmail");
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxPassword
+            // 
+            resources.ApplyResources(this.textBoxPassword, "textBoxPassword");
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // buttonMostra
+            // 
+            resources.ApplyResources(this.buttonMostra, "buttonMostra");
+            this.buttonMostra.Name = "buttonMostra";
+            this.buttonMostra.UseVisualStyleBackColor = true;
+            this.buttonMostra.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonMostra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Register);
+            this.Controls.Add(this.Login);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button Register;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonMostra;
     }
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -7,12 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
 
 using System.Net;
 using System.Net.Sockets;
-
-
-
+using System.Windows;
+using MessageBox = System.Windows.Forms.MessageBox;
+using System.Net.Mail;
+using System.Data.SqlClient;
 
 namespace Client
 {
@@ -28,36 +32,135 @@ namespace Client
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-             send = Connection.StartClient();
-            Console.WriteLine("load caricata correttamente");
-            
-            Console.WriteLine("Socket connected to {0}",
-                    send.RemoteEndPoint.ToString());
+            // Set the start position of the form to the center of the screen.
+            //Form.StartPosition = FormStartPosition.CenterScreen;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Console.WriteLine("prova1");
+            String email;
+            String password;
 
-            byte[] bytes = new byte[1024];
+            if (textBoxEmail.Text != string.Empty && textBoxPassword.Text != string.Empty)
+            {
 
-            // Encode the data string into a byte array.
-            byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+                email = textBoxEmail.Text;
+                password = textBoxPassword.Text;
 
-            // Send the data through the socket.
-            int bytesSent = send.Send(msg);
-
-            // Receive the response from the remote device.
-            int bytesRec = send.Receive(bytes);
-            Console.WriteLine("Echoed test = {0}",
-                Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                Console.WriteLine("EMAIL " + textBoxEmail.Text + " Password " + textBoxPassword.Text);
 
 
+                bool isValidEmail1 = email.Contains("@");
+                bool isValidEmail2 = email.Contains(".");
+                if (!isValidEmail1 || !isValidEmail2)
+                {
+                    Console.WriteLine($"The email is invalid");
+                    
+                    MessageBox.Show("Email non valida", "Errore");
+                }
+                else
+                {
+                    Console.WriteLine($"The email is valid");
+
+                    //------------database---------------------------------------------
+                    string server = "localhost";
+                    string database = "apl_database";
+                    string uid = "Dario";
+                    string passworddatabase = "ciaociao99";
+                    string connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + passworddatabase + ";";
+
+                    MySqlConnection con = new MySqlConnection(connectionString);
+
+                    con.Open();
+
+                    var stm = "SELECT COUNT(*) FROM apl_database.utenti WHERE Email='" + email+ "' AND Password='"+password+"'";
+                    var cmd = new MySqlCommand(stm, con);
+                    int risultato = int.Parse(cmd.ExecuteScalar().ToString());
+                    if (risultato == 0) {
+                        Console.WriteLine("Utente non trovato");
+                    }
+                    if (risultato == 1)
+                    {
+                        Console.WriteLine("Utente trovato");
+                    }
+                    
+                    //Console.WriteLine("cmd: "+ cmd.ExecuteScalar().ToString());
+                    //----------------------------------------------------------------
 
 
-            send.Shutdown(SocketShutdown.Both);
-            send.Close();
+
+                    textBoxEmail.Text = string.Empty;
+                    textBoxPassword.Text = string.Empty;
+                }
+
+
+
+                
+                
+
+            }
+            else {
+                
+
+                // disattiva la X in alto a destra
+                // MessageBoxButtons buttons = MessageBoxButtons.AbortRetryIgnore;
+                MessageBox.Show("Riempire tutti i campi", "Errore");
+
+
+            }
+            
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+           
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Add this textbox to form
+            this.Controls.Add(textBoxPassword);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (textBoxPassword.PasswordChar == default) { 
+            textBoxPassword.PasswordChar = '*'; }
+            else { 
+                textBoxPassword.PasswordChar = default; }
+
+            
+
 
         }
     }
