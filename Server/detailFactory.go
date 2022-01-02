7c2e3296e52ca3691111740a522ce2f69a5c9ee0
Nexkia <giuseppe794@yahoo.it>
2022-01-02 16:05:00 +0100

    confronto sistemato

---
+++ Server/detailFactory.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func getDetailType(detailType string) (interface{}, error) {
+
+	switch detailType {
+	case "cpu":
+		return &CpuDetail{}, nil
+	case "schdaVideo":
+		return &SchedaVideoDetail{}, nil
+	case "schedaMadre":
+		return &SchedaMadreDetail{}, nil
+	case "memoria":
+		return &MemoriaDetail{}, nil
+	case "alimentatore":
+		return &AlimentatoreDetail{}, nil
+	case "dissipatore":
+		return &DissipatoreDetail{}, nil
+	case "ram":
+		return &RamDetail{}, nil
+	case "casepc":
+		return &CasePCDetail{}, nil
+
+	default:
+		return nil, fmt.Errorf("error")
+	}
+}
