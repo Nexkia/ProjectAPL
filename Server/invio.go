d0366817d710b7077f66a2aa114762a5ce75712e
Nexkia <giuseppe794@yahoo.it>
2022-01-04 18:00:14 +0100

    limit 80

--- Server/invio.go
+++ Server/invio.go
@@ -97,6 +97,7 @@ func invio() {
 	max_val := 10
 	min_ram := 3
 	max_ram := 4
+	limit := 80
 	content, err := ioutil.ReadFile("db/case.txt")
 	if err != nil {
 		fmt.Println("err")
@@ -106,23 +107,25 @@ func invio() {
 	cspc := Componente{}
 	cspc_detail := CasePCDetail{}
 	var flag_check bool
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			cspc.Categoria = "casepc"
-			cspc.Marca = elem[1]
-			cspc.Modello = modello
-			if elem[5] != "" {
-				cspc.Prezzo, _ = strconv.ParseFloat(elem[5], 64)
-			} else {
-				cspc.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				cspc.Categoria = "casepc"
+				cspc.Marca = elem[1]
+				cspc.Modello = modello
+				if elem[5] != "" {
+					cspc.Prezzo, _ = strconv.ParseFloat(elem[5], 64)
+				} else {
+					cspc.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				cspc_detail.Modello = modello
+				cspc_detail.Taglia = elem[3]
+				cspc_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), cspc)
+				coll_detail.InsertOne(context.TODO(), cspc_detail)
 			}
-			cspc_detail.Modello = modello
-			cspc_detail.Taglia = elem[3]
-			cspc_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), cspc)
-			coll_detail.InsertOne(context.TODO(), cspc_detail)
 		}
 	}
 	content, err = ioutil.ReadFile("db/cpu.txt")
@@ -133,26 +136,28 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	cpu := Componente{}
 	cpu_detail := CpuDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			cpu.Categoria = "cpu"
-			cpu.Marca = elem[1]
-			cpu.Modello = modello
-			if elem[8] != "" {
-				cpu.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
-			} else {
-				cpu.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				cpu.Categoria = "cpu"
+				cpu.Marca = elem[1]
+				cpu.Modello = modello
+				if elem[8] != "" {
+					cpu.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
+				} else {
+					cpu.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				cpu_detail.Modello = modello
+				cpu_detail.Socket = elem[3]
+				cpu_detail.Frequenza, _ = strconv.ParseFloat(elem[4], 64)
+				cpu_detail.Core, _ = strconv.Atoi(elem[6])
+				cpu_detail.Thread, _ = strconv.Atoi(elem[7])
+				cpu_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), cpu)
+				coll_detail.InsertOne(context.TODO(), cpu_detail)
 			}
-			cpu_detail.Modello = modello
-			cpu_detail.Socket = elem[3]
-			cpu_detail.Frequenza, _ = strconv.ParseFloat(elem[4], 64)
-			cpu_detail.Core, _ = strconv.Atoi(elem[6])
-			cpu_detail.Thread, _ = strconv.Atoi(elem[7])
-			cpu_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), cpu)
-			coll_detail.InsertOne(context.TODO(), cpu_detail)
 		}
 	}
 
@@ -164,24 +169,26 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	gpu := Componente{}
 	gpu_detail := SchedaVideoDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			gpu.Categoria = "schedaVideo"
-			gpu.Marca = elem[1]
-			gpu.Modello = modello
-			if elem[8] != "" {
-				gpu.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
-			} else {
-				gpu.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				gpu.Categoria = "schedaVideo"
+				gpu.Marca = elem[1]
+				gpu.Modello = modello
+				if elem[8] != "" {
+					gpu.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
+				} else {
+					gpu.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				gpu_detail.Modello = modello
+				gpu_detail.Vram, _ = strconv.Atoi(elem[4])
+				gpu_detail.Tdp, _ = strconv.Atoi(elem[6])
+				gpu_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), gpu)
+				coll_detail.InsertOne(context.TODO(), gpu_detail)
 			}
-			gpu_detail.Modello = modello
-			gpu_detail.Vram, _ = strconv.Atoi(elem[4])
-			gpu_detail.Tdp, _ = strconv.Atoi(elem[6])
-			gpu_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), gpu)
-			coll_detail.InsertOne(context.TODO(), gpu_detail)
 		}
 	}
 
@@ -193,23 +200,25 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	psu := Componente{}
 	psu_detail := AlimentatoreDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			psu.Categoria = "alimentatore"
-			psu.Marca = elem[1]
-			psu.Modello = modello
-			if elem[5] != "" {
-				psu.Prezzo, _ = strconv.ParseFloat(elem[5], 64)
-			} else {
-				psu.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				psu.Categoria = "alimentatore"
+				psu.Marca = elem[1]
+				psu.Modello = modello
+				if elem[5] != "" {
+					psu.Prezzo, _ = strconv.ParseFloat(elem[5], 64)
+				} else {
+					psu.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				psu_detail.Modello = modello
+				psu_detail.Watt, _ = strconv.Atoi(elem[3])
+				psu_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), psu)
+				coll_detail.InsertOne(context.TODO(), psu_detail)
 			}
-			psu_detail.Modello = modello
-			psu_detail.Watt, _ = strconv.Atoi(elem[3])
-			psu_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), psu)
-			coll_detail.InsertOne(context.TODO(), psu_detail)
 		}
 	}
 
@@ -221,25 +230,28 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	ram := Componente{}
 	ram_detail := RamDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			ram.Categoria = "ram"
-			ram.Marca = elem[1]
-			ram.Modello = modello
-			ram.Capienza, _ = strconv.Atoi(elem[3])
-			if elem[8] != "" {
-				ram.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
-			} else {
-				ram.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				ram.Categoria = "ram"
+				ram.Marca = elem[1]
+				ram.Modello = modello
+				ram.Capienza, _ = strconv.Atoi(elem[3])
+				if elem[8] != "" {
+					ram.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
+				} else {
+					ram.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				ram_detail.Modello = modello
+				ram_detail.Standard = elem[5]
+				ram_detail.Frequenza, _ = strconv.Atoi(elem[6])
+				ram_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), ram)
+				coll_detail.InsertOne(context.TODO(), ram_detail)
 			}
-			ram_detail.Modello = modello
-			ram_detail.Standard = elem[5]
-			ram_detail.Frequenza, _ = strconv.Atoi(elem[6])
-			ram_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), ram)
-			coll_detail.InsertOne(context.TODO(), ram_detail)
 		}
 	}
 	content, err = ioutil.ReadFile("db/hdd.txt")
@@ -250,25 +262,27 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	hdd := Componente{}
 	hdd_detail := MemoriaDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			hdd.Categoria = "memoria"
-			hdd.Marca = elem[1]
-			hdd.Modello = modello
-			hdd.Capienza, _ = strconv.Atoi(elem[3])
-			hdd.Capienza = hdd.Capienza * 1000
-			if elem[6] != "" {
-				ram.Prezzo, _ = strconv.ParseFloat(elem[6], 64)
-			} else {
-				ram.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				hdd.Categoria = "memoria"
+				hdd.Marca = elem[1]
+				hdd.Modello = modello
+				hdd.Capienza, _ = strconv.Atoi(elem[3])
+				hdd.Capienza = hdd.Capienza * 1000
+				if elem[6] != "" {
+					ram.Prezzo, _ = strconv.ParseFloat(elem[6], 64)
+				} else {
+					ram.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				hdd_detail.Modello = modello
+				hdd_detail.Tipo = "hdd"
+				hdd_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), hdd)
+				coll_detail.InsertOne(context.TODO(), hdd_detail)
 			}
-			hdd_detail.Modello = modello
-			hdd_detail.Tipo = "hdd"
-			hdd_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), hdd)
-			coll_detail.InsertOne(context.TODO(), hdd_detail)
 		}
 	}
 	content, err = ioutil.ReadFile("db/ssd.txt")
@@ -279,24 +293,27 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	ssd := Componente{}
 	ssd_detail := MemoriaDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			ssd.Categoria = "memoria"
-			ssd.Marca = elem[1]
-			ssd.Modello = modello
-			ssd.Capienza, _ = strconv.Atoi(elem[3])
-			if elem[8] != "" {
-				ram.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
-			} else {
-				ram.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				ssd.Categoria = "memoria"
+				ssd.Marca = elem[1]
+				ssd.Modello = modello
+				ssd.Capienza, _ = strconv.Atoi(elem[3])
+				if elem[8] != "" {
+					ram.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
+				} else {
+					ram.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				ssd_detail.Modello = modello
+				ssd_detail.Tipo = "ssd"
+				ssd_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), ssd)
+				coll_detail.InsertOne(context.TODO(), ssd_detail)
 			}
-			ssd_detail.Modello = modello
-			ssd_detail.Tipo = "ssd"
-			ssd_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), ssd)
-			coll_detail.InsertOne(context.TODO(), ssd_detail)
 		}
 	}
 	content, err = ioutil.ReadFile("db/cpucooler.txt")
@@ -307,32 +324,34 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	cpucooler := Componente{}
 	cpucooler_detail := DissipatoreDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			cpucooler.Categoria = "dissipatore"
-			cpucooler.Marca = elem[1]
-			cpucooler.Modello = modello
-			if elem[7] != "" {
-				cpucooler.Prezzo, _ = strconv.ParseFloat(elem[7], 64)
-			} else {
-				cpucooler.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
-			}
-			cpucooler_detail.Modello = modello
-			listSocket := strings.Split(elem[3], ", ")
-			listSocketL := make([]string, len(listSocket)+1)
-			for i := 0; i < len(listSocketL); i++ {
-				if i < len(listSocket) {
-					listSocketL[i] = listSocket[i]
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				cpucooler.Categoria = "dissipatore"
+				cpucooler.Marca = elem[1]
+				cpucooler.Modello = modello
+				if elem[7] != "" {
+					cpucooler.Prezzo, _ = strconv.ParseFloat(elem[7], 64)
 				} else {
-					listSocketL[i] = elem[4]
+					cpucooler.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
 				}
+				cpucooler_detail.Modello = modello
+				listSocket := strings.Split(elem[3], ", ")
+				listSocketL := make([]string, len(listSocket)+1)
+				for i := 0; i < len(listSocketL); i++ {
+					if i < len(listSocket) {
+						listSocketL[i] = listSocket[i]
+					} else {
+						listSocketL[i] = elem[4]
+					}
+				}
+				cpucooler_detail.CpuSocket = listSocketL
+				cpucooler_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), cpucooler)
+				coll_detail.InsertOne(context.TODO(), cpucooler_detail)
 			}
-			cpucooler_detail.CpuSocket = listSocketL
-			cpucooler_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), cpucooler)
-			coll_detail.InsertOne(context.TODO(), cpucooler_detail)
 		}
 	}
 	content, err = ioutil.ReadFile("db/motherboard.txt")
@@ -343,26 +362,28 @@ func invio() {
 	result = reg.FindAllStringSubmatch(string(content), -1)
 	mb := Componente{}
 	mb_detail := SchedaMadreDetail{}
-	for _, elem := range result {
-		modello := strings.TrimSuffix(elem[2], " ")
-		flag_check = controlloModello(modello, coll_comp)
-		if flag_check {
-			mb.Categoria = "schedaMadre"
-			mb.Marca = elem[1]
-			mb.Modello = modello
-			if elem[8] != "" {
-				mb.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
-			} else {
-				mb.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+	for i, elem := range result {
+		if i < limit {
+			modello := strings.TrimSuffix(elem[2], " ")
+			flag_check = controlloModello(modello, coll_comp)
+			if flag_check {
+				mb.Categoria = "schedaMadre"
+				mb.Marca = elem[1]
+				mb.Modello = modello
+				if elem[8] != "" {
+					mb.Prezzo, _ = strconv.ParseFloat(elem[8], 64)
+				} else {
+					mb.Prezzo = float64(rand.Intn((max_price - min_price)) + min_price)
+				}
+				mb_detail.Modello = modello
+				mb_detail.CpuSocket = elem[3]
+				mb_detail.Chipset = elem[6]
+				ram_number := rand.Intn((max_ram - min_ram)) + min_ram
+				mb_detail.Ram = "DDR" + strconv.Itoa(ram_number)
+				mb_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
+				coll_comp.InsertOne(context.TODO(), mb)
+				coll_detail.InsertOne(context.TODO(), mb_detail)
 			}
-			mb_detail.Modello = modello
-			mb_detail.CpuSocket = elem[3]
-			mb_detail.Chipset = elem[6]
-			ram_number := rand.Intn((max_ram - min_ram)) + min_ram
-			mb_detail.Ram = "DDR" + strconv.Itoa(ram_number)
-			mb_detail.Valutazione = rand.Intn((max_val - min_val)) + min_val
-			coll_comp.InsertOne(context.TODO(), mb)
-			coll_detail.InsertOne(context.TODO(), mb_detail)
 		}
 	}
 }
